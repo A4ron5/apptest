f58fce0ed0f2e003f5d65dd8da76e2a0b6dbbd76
Max <a4ron5@yandex.ru>
2018-05-03 16:38:51 +0500

    feat(app): fix names

--- src/features/app/app.js
+++ src/features/app/app.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 import { Header } from '../../ui/organisms'
 import { Container } from '../../ui/atoms'
+import { logOUT } from '../login/action'
+
 
 const AppRaw = (props) => (
   <Container>
-    <Header status={props.status}/>
+    <Header 
+      isLogin={props.isLogin} 
+      onClick={() => props.logOUT()} 
+    />
     {props.children}
   </Container>
 )
 
 const mapStateToProps = (state) => {
   return {
-    status: state.login.login.status
+    isLogin: state.login.isAuth,
   }
 }
 
-export const App = connect(mapStateToProps, null)(AppRaw)
+export const App = withRouter(connect(mapStateToProps, { logOUT })(AppRaw))
